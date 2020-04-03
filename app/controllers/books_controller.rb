d9486b8edde9a0d64a4b4544587e4ee7b31735dc
hojokaban <hojokaban@gmail.com>
2020-04-03 22:11:34 +0900

    [AllPagesConnected]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,6 +22,14 @@ class BooksController < ApplicationController
   end
 
   def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	book.update(book_params)
+  	flash[:notice] = "Book was successfully updated."
+  	redirect_to book_path(book)
   end
 
   private
